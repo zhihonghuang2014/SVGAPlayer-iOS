98ef0caedefedf37c9294b20042040ccb2276ad6
zhihonghuang2014 <zhihonghuang@foxmail.com>
2023-12-09 10:53:08 +0800

    Update Svga.pbobjc.m
    #import <libkern/OSAtomic.h>

--- Source/pbobjc/Svga.pbobjc.m
+++ Source/pbobjc/Svga.pbobjc.m
@@ -15,6 +15,7 @@
 
 #import "Svga.pbobjc.h"
 // @@protoc_insertion_point(imports)
+#import <libkern/OSAtomic.h>
 
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
